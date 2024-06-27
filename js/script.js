1d4f5f43e5d9c97750eaed9458e1261e473a93f1
RianRafi <2220563@iub.edu.bd>
2024-06-27 13:02:06 +0600

    made changes

--- js/script.js
+++ js/script.js
@@ -3,15 +3,15 @@
 var sidebarOpen = false;
 var sidebar = document.getElementById("sidebar");
 
-function openSidebar(){
-    if(!sidebarOpen){
+function openSidebar() {
+    if (!sidebarOpen) {
         sidebar.classList.add("sidebar-responsive");
         sidebarOpen = true;
     }
 }
 
-function closeSidebar(){
-    if(sidebarOpen){
+function closeSidebar() {
+    if (sidebarOpen) {
         sidebar.classList.remove("sidebar-responsive");
         sidebarOpen = false;
     }
@@ -21,110 +21,232 @@ function closeSidebar(){
 
 //---------bar chart-----------//
 
-
-    const barChartOptions = {
-      series: [
+const barChartOptions = {
+    series: [
         {
-          data: [10, 8, 6, 4, 2],
-          name: 'Products',
+            data: [10, 8, 6, 4, 2],
+            name: 'last week',
         },
-      ],
-      chart: {
+    ],
+    chart: {
         type: 'bar',
         background: 'transparent',
         height: 350,
         toolbar: {
-          show: false,
+            show: false,
         },
-      },
-      colors: ['#2962ff', '#d50000', '#2e7d32', '#ff6d00', '#583cb3'],
-      plotOptions: {
+    },
+    colors: ['#2962ff', '#d50000', '#2e7d32', '#ff6d00', '#583cb3'],
+    plotOptions: {
         bar: {
-          distributed: true,
-          borderRadius: 4,
-          horizontal: false,
-          columnWidth: '40%',
+            distributed: true,
+            borderRadius: 4,
+            horizontal: false,
+            columnWidth: '40%',
         },
-      },
-      dataLabels: {
+    },
+    dataLabels: {
         enabled: false,
-      },
-      fill: {
+    },
+    fill: {
         opacity: 1,
-      },
-      grid: {
+    },
+    grid: {
         borderColor: '#55596e',
         yaxis: {
-          lines: {
-            show: true,
-          },
+            lines: {
+                show: true,
+            },
         },
         xaxis: {
-          lines: {
-            show: true,
-          },
+            lines: {
+                show: true,
+            },
         },
-      },
-      legend: {
+    },
+    legend: {
         labels: {
-          colors: '#f5f7ff',
+            colors: '#f5f7ff',
         },
         show: true,
         position: 'top',
-      },
-      stroke: {
+    },
+    stroke: {
         colors: ['transparent'],
         show: true,
         width: 2,
-      },
-      tooltip: {
+    },
+    tooltip: {
         shared: true,
         intersect: false,
         theme: 'dark',
+    },
+    xaxis: {
+        categories: ['Jogging', 'Sprint', 'Cycling', 'Weight Lift', 'Swimming'],
+        title: {
+            style: {
+                color: '#f5f7ff',
+            },
+        },
+        axisBorder: {
+            show: true,
+            color: '#55596e',
+        },
+        axisTicks: {
+            show: true,
+            color: '#55596e',
+        },
+        labels: {
+            style: {
+                colors: '#f5f7ff',
+            },
+        },
+    },
+    yaxis: {
+        title: {
+            text: 'Hours Spent',
+            style: {
+                color: '#f5f7ff',
+            },
+        },
+        axisBorder: {
+            color: '#55596e',
+            show: true,
+        },
+        axisTicks: {
+            color: '#55596e',
+            show: true,
+        },
+        labels: {
+            style: {
+                colors: '#f5f7ff',
+            },
+        },
+    },
+};
+
+var chart = new ApexCharts(document.querySelector(".bar-chart"), barChartOptions);
+chart.render();
+
+
+// AREA CHART
+  const areaChartOptions = {
+    series: [
+      {
+        name: 'Purchase Orders',
+        data: [31, 40, 28, 51, 42, 109, 100],
+      },
+      {
+        name: 'Sales Orders',
+        data: [11, 32, 45, 32, 34, 52, 41],
+      },
+    ],
+    chart: {
+      type: 'area',
+      background: 'transparent',
+      height: 350,
+      stacked: false,
+      toolbar: {
+        show: false,
+      },
+    },
+    colors: ['#00ab57', '#d50000'],
+    labels: ['Jan', 'Feb', 'Mar', 'Apr', 'May', 'Jun', 'Jul'],
+    dataLabels: {
+      enabled: false,
+    },
+    fill: {
+      gradient: {
+        opacityFrom: 0.4,
+        opacityTo: 0.1,
+        shadeIntensity: 1,
+        stops: [0, 100],
+        type: 'vertical',
+      },
+      type: 'gradient',
+    },
+    grid: {
+      borderColor: '#55596e',
+      yaxis: {
+        lines: {
+          show: true,
+        },
       },
       xaxis: {
-        categories: ['Laptop', 'Phone', 'Monitor', 'Headphones', 'Camera'],
+        lines: {
+          show: true,
+        },
+      },
+    },
+    legend: {
+      labels: {
+        colors: '#f5f7ff',
+      },
+      show: true,
+      position: 'top',
+    },
+    markers: {
+      size: 6,
+      strokeColors: '#1b2635',
+      strokeWidth: 3,
+    },
+    stroke: {
+      curve: 'smooth',
+    },
+    xaxis: {
+      axisBorder: {
+        color: '#55596e',
+        show: true,
+      },
+      axisTicks: {
+        color: '#55596e',
+        show: true,
+      },
+      labels: {
+        offsetY: 5,
+        style: {
+          colors: '#f5f7ff',
+        },
+      },
+    },
+    yaxis: [
+      {
         title: {
+          text: 'Purchase Orders',
           style: {
             color: '#f5f7ff',
           },
         },
-        axisBorder: {
-          show: true,
-          color: '#55596e',
-        },
-        axisTicks: {
-          show: true,
-          color: '#55596e',
-        },
         labels: {
           style: {
-            colors: '#f5f7ff',
+            colors: ['#f5f7ff'],
           },
         },
       },
-      yaxis: {
+      {
+        opposite: true,
         title: {
-          text: 'Count',
+          text: 'Sales Orders',
           style: {
             color: '#f5f7ff',
           },
         },
-        axisBorder: {
-          color: '#55596e',
-          show: true,
-        },
-        axisTicks: {
-          color: '#55596e',
-          show: true,
-        },
         labels: {
           style: {
-            colors: '#f5f7ff',
+            colors: ['#f5f7ff'],
           },
         },
       },
-    };
-
-    var barChart = new ApexCharts(document.querySelector(".bar-chart"), barChartOptions);
-    barChart.render();
+    ],
+    tooltip: {
+      shared: true,
+      intersect: false,
+      theme: 'dark',
+    },
+  };
+  
+  const areaChart = new ApexCharts(
+    document.querySelector('.area-chart'),
+    areaChartOptions
+  );
+  areaChart.render();
